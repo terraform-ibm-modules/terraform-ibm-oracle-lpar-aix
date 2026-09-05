8fb1bd0c0e0f38add73ed79c4ccb688fabaac8f5
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-09-05 08:49:39 +0530

    fix(deps): update terraform terraform-ibm-modules/powervs-infrastructure/ibm to v11.4.0 (#57)

--- solutions/oracle/rac-ready-to-go/main.tf
+++ solutions/oracle/rac-ready-to-go/main.tf
@@ -19,7 +19,7 @@
 
 module "landing_zone" {
   source  = "terraform-ibm-modules/powervs-infrastructure/ibm//modules/powervs-vpc-landing-zone"
-  version = "11.3.0"
+  version = "11.4.0"
 
   providers = {
     ibm.ibm-is = ibm.ibm-is
